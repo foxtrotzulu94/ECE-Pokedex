8788938d7fd7af47c0ffb5332f00e50602b6220e
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-02-22 10:06:40 -0500

    Added background image and other UI refinements

--- app/src/main/java/com/hackers/intj/mchackspokedex/MainActivity.java
+++ app/src/main/java/com/hackers/intj/mchackspokedex/MainActivity.java
@@ -35,11 +35,11 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        TextView t;
-
-
-        t = (TextView) findViewById(R.id.name);
-        t.setText("TESTY");
+//        TextView t;
+//
+//
+//        t = (TextView) findViewById(R.id.name);
+//        t.setText("The COENDex");
         return true;
     }
 
