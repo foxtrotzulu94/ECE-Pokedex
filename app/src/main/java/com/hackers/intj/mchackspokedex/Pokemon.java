5417c603dcc47e5cd0f3a0991246c02d668a18b9
Thinesh <th.thinesh@gmail.com>
2015-02-21 20:38:29 -0500

    refactoring pokemon.java

---
+++ app/src/main/java/com/hackers/intj/mchackspokedex/Pokemon.java
@@ -0,0 +1,43 @@
+package com.hackers.intj.mchackspokedex;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by Thinesh on 2015-02-21.
+ */
+public class Pokemon extends SugarRecord<Pokemon> {
+
+    public Pokemon(){
+
+    }
+
+
+
+    public Pokemon(int pkdx_id, String name){
+        String a =this.description;
+        this.pkdx_id = pkdx_id;
+        this.name = name;
+    }
+
+    String name;
+    String description;
+    int pkdx_id;
+    String weight;
+    String height;
+
+    Ability[] abilities;
+    Type Type1;
+    Type Type2;
+
+    int hp;
+    int attack;
+    int defense;
+    int sp_att;
+    int sp_def;
+
+
+
+
+
+
+}
