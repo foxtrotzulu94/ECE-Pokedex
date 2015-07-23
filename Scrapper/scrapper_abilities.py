9255d62e2ff4e531e62358959da3f7fb2b2e6c5e
flareflare <am_desa@encs.concordia.ca>
2015-07-23 17:11:45 -0400

    #4-Scrapper: Scrapper json files - abilities, pokemon, smogon

--- Scrapper/scrapper_abilities.py
+++ Scrapper/scrapper_abilities.py
@@ -1,1 +1,32 @@
 __author__ = 'as'
+
+import bs4;
+import sqlite3;
+import json;
+
+
+#Connect to SQLite
+# conn = sqlite3.connect('..//database/pokedex')
+#
+# c = conn.cursor();
+#
+# c.execute("delete from " + "abilities")
+#
+# c.execute("INSERT INTO abilities VALUES (1,'ditto', 'an egg')")
+#
+#
+# conn.commit()
+#
+# conn.close()
+
+
+# conn = sqlite3.connect('..//database/pokedex')
+# c=conn.cursor()
+# c.execute("INSERT INTO typeEffectiveness VALUES (1,'NaN',2)")
+# conn.commit()
+# conn.close()
+
+# json.load('pokemon.json')
+
+with open("pokemon.json") as data_file:
+    data = json.load(data_file)
