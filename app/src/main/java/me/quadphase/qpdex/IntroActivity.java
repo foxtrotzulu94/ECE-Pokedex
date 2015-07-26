a3f09a77def36e5db7835ad74d37845b9a60c780
foxtrot94 <j_fajard@encs.concordia.ca>
2015-07-26 00:59:11 -0400

    Issue #8: WIP Activity done and Build Date Display Added.

--- app/src/main/java/me/quadphase/qpdex/IntroActivity.java
+++ app/src/main/java/me/quadphase/qpdex/IntroActivity.java
@@ -1,6 +1,7 @@
 package me.quadphase.qpdex;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.PixelFormat;
@@ -8,12 +9,16 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
 import android.content.res.Configuration;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -23,6 +28,7 @@ public class IntroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_intro);
 //        Window ourWin = getWindow();
 //        ourWin.setFormat(PixelFormat.R);
+        //Set the background by manually calling the bitmap decoder.
         BitmapFactory.Options op = new BitmapFactory.Options();
         op.inPreferredConfig = Bitmap.Config.RGB_565;
         op.inSampleSize = 1;
@@ -31,6 +37,13 @@ public class IntroActivity extends AppCompatActivity {
         op.inScaled = true;
         LinearLayout lin = (LinearLayout)findViewById(R.id.lin_back);
         lin.setBackground(new BitmapDrawable(getResources(), BitmapFactory.decodeResource(getResources(), R.drawable.menubackground, op)));
+
+        //Show build date (DEVELOPMENT ONLY! REMOVE WHEN RELEASE)
+        TextView buildField = (TextView) findViewById(R.id.app_build);
+        Date buildDate = new Date(BuildConfig.TIMESTAMP);
+        buildField.setText("Built: "+buildDate.toString()+" ["+BuildConfig.BUILD_TYPE+"]");
+
+        //Signal for collection if needed
         Runtime.getRuntime().gc();
         System.gc();
     }
@@ -59,6 +72,11 @@ public class IntroActivity extends AppCompatActivity {
 
     public void switchToPokedex(View view){
         Intent intent = new Intent(this,PokedexActivity.class);
+        startActivity(intent);
+    }
+
+    public void showConstructionActivity(View view){
+        Intent intent = new Intent(this,WIPActivity.class);
         startActivity(intent);
     }
 }
