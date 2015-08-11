55e2d97a3a5143a0fa728c68c60a8a5c61552f0a
flareflare <am_desa@encs.concordia.ca>
2015-08-11 16:59:57 -0400

    issue-#4-Scrapper:
    In scrapper_pokemonTypes: pokemon_abilities table completed to match pokemon with their abilities.

--- Scrapper/scrapper_pokemonTypes.py
+++ Scrapper/scrapper_pokemonTypes.py
@@ -1,5 +1,11 @@
 __author__ = 'as'
 
+# This file:
+# 1) finds the generation the pokemon first appeared in,
+# 2) links the pokemonID to NationalID,
+# 3) links type to typeID
+# 4) links ability to abilityID
+
 import json
 import bs4
 import sqlite3
@@ -16,6 +22,9 @@ c = conn.cursor();
 
 c.execute("delete from " + "pokemon_types")
 c.execute("delete from " + " sqlite_sequence where name = 'pokemon_types'")
+
+c.execute("delete from " + "pokemon_abilities")
+c.execute("delete from " + " sqlite_sequence where name = 'pokemon_abilities'")
 
 
 uniqueID = 0
@@ -55,6 +64,18 @@ for pokemons in data["pokemon"]:
             pokemon_Types = c.fetchone()
             c.execute("INSERT INTO pokemon_types VALUES (?,?)", (uniqueID, pokemon_Types[0]))
             print(pokemon_Types[0])
+
+
+
+        # Abilities that a Pokemon has
+        abilitiesList = pokemonForm["abilities"]
+
+        for abilities in abilitiesList:
+            abilitiesTuple = (abilities,)
+            pokemon_Abilities_List = c.execute("select * from abilities WHERE name=?", abilitiesTuple)
+            pokemon_Abilities = c.fetchone()
+            c.execute("INSERT INTO pokemon_abilities VALUES (?,?)", (uniqueID, pokemon_Abilities[0]))
+            print(pokemon_Abilities[1])
 
 
         conn.commit()
